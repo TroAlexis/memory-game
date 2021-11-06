ab2748fdaf43737edfd76145cb0d30ea9d2a0af7
Alex Troshin <a.troshin@idaproject.com>
2021-11-06 12:29:38 +0300

    feat(eslint): ignore no-explicit-any rule for rest args

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/script-setup-uses-vars': 'error',
     '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': ['error', {'ignoreRestArgs': true}]
   },
 };
